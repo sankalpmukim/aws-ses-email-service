54ad57277e3927b75fec9a714bbd93d6e6ac0048
Sankalp Mukim <sankalpmukim@gmail.com>
2023-04-11 18:56:54 +0530

    lambda experiment

--- src/index.ts
+++ src/index.ts
@@ -1,58 +1,64 @@
-import "./configEnvironment";
-import express, { Express, Request, Response } from "express";
-import bodyParser from "body-parser";
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyHandler,
+  APIGatewayProxyResult,
+} from "aws-lambda";
 import { sendBulkTransactionalEmail, sendTransactionalEmail } from "./aws-ses";
 
-const app: Express = express();
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// health check
-app.get("/", (req: Request, res: Response) => {
-  return res.sendStatus(200);
-});
-
-// Transactional email endpoint (one to one)
-app.post("/send-email", async (req: Request, res: Response) => {
+export const sendEmail: APIGatewayProxyHandler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
   try {
-    const { to, subject, body } = req.body;
-    // input validation
-    if (!to || !subject || !body) {
-      return res.status(400).send("Invalid input");
-    }
-    const data = await sendTransactionalEmail({ to, subject, body });
+    const { to, subject, body } = JSON.parse(event.body || "{}");
 
-    console.log(`⚡️[aws]:`, data);
-    return res.status(200).send(data);
+    if (!to || !subject || !body) {
+      return {
+        statusCode: 400,
+        body: "Invalid input",
+      };
+    }
+
+    const data = await sendTransactionalEmail({ to, subject, body });
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data),
+    };
   } catch (error) {
     console.log(error);
-    return res.sendStatus(500);
+    return {
+      statusCode: 500,
+      body: "Internal Server Error",
+    };
   }
-});
+};
 
-// Marketing email endpoint (one to many)
-app.post("/send-bulk-email", async (req: Request, res: Response) => {
+export const sendBulkEmail: APIGatewayProxyHandler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
   try {
-    const { recipients, subject, body } = req.body;
-    // input validation
+    const { recipients, subject, body } = JSON.parse(event.body || "{}");
+
     if (!recipients || !subject || !body) {
-      return res.status(400).send("Invalid input");
+      return {
+        statusCode: 400,
+        body: "Invalid input",
+      };
     }
+
     const data = await sendBulkTransactionalEmail({
       recipients,
       subject,
       body,
     });
-    console.log(`⚡️[aws]:`, data);
-    return res.status(200).send(data);
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data),
+    };
   } catch (error) {
     console.log(error);
-    return res.sendStatus(500);
+    return {
+      statusCode: 500,
+      body: "Internal Server Error",
+    };
   }
-});
-
-const port = process.env.PORT || 8000;
-app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
-});
+};
